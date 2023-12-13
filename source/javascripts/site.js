dd005d4fd2e0b411671b4b5bbfcf7274fbc80fc5
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2023-12-13 19:29:30 +0900

    Size changed in the parallax bg

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,5 +1,5 @@
 window.addEventListener("scroll", () => {
-  document.querySelector(".banner-bg").style.backgroundSize = `${window.scrollY * 1.8 + 1600 }px`
+  document.querySelector(".banner-bg").style.backgroundSize = `${window.scrollY * 1.8 + 2000 }px`
   document.querySelector(".banner-bg h1").style.opacity = `${(-window.scrollY + 300) * .004}`
   document.querySelector(".banner-bg p").style.opacity = `${(-window.scrollY + 300) * .004}`
   document.querySelector(".banner-bg i").style.opacity = `${(-window.scrollY + 300) * .004}`
