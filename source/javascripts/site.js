5fc56f825b805e15d299ef1bb0a5d0498370042d
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2023-12-14 18:14:45 +0900

    stars effect

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -4,3 +4,26 @@ window.addEventListener("scroll", () => {
   document.querySelector(".banner-bg p").style.opacity = `${(-window.scrollY + 300) * .004}`
   document.querySelector(".banner-bg i").style.opacity = `${(-window.scrollY + 300) * .004}`
 })
+
+// bg stars
+
+function stars() {
+  let e = document.createElement("div");
+  e.setAttribute("class", "star");
+  document.body.appendChild(e);
+  e.style.left = Math.random() * + innerWidth + "px";
+
+  let size = Math.random() * 12;
+  let duration = Math.random() * 3;
+
+  e.style.fontSize = 12 + size + "px";
+  e.style.animationDuration = 2 + duration + "s";
+
+  setTimeout(() => {
+    document.body.removeChild(e);
+  }, 5000);
+}
+
+setInterval(() => {
+ stars()
+}, 100);
