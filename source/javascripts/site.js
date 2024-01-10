5fa03e51e70894d02eb6d676f3e826c02ed47d1f
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2024-01-10 18:27:52 +0900

    On-scroll reveal functional

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -29,19 +29,3 @@ function stars() {
 setInterval(() => {
  stars()
 }, 230);
-
-// Scroll animation
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      console.log(entry)
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show");
-      } else {
-        entry.target.classList.remove("show");
-      }
-    });
-});
-
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
