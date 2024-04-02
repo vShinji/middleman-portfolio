2907e0973c9a8a132ea0df3dc66785d2bcc501b5
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2024-04-02 16:58:06 +0900

    cards working in smaller devices

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,11 +1,11 @@
 // Parallax bg
 
-// window.addEventListener("scroll", () => {
-//   document.querySelector(".banner-bg").style.backgroundSize = `${window.scrollY * 1.8 + 2000 }px`
-//   document.querySelector(".banner-bg h1").style.opacity = `${(-window.scrollY + 300) * .004}`
-//   document.querySelector(".banner-bg p").style.opacity = `${(-window.scrollY + 300) * .004}`
-//   document.querySelector(".banner-bg i").style.opacity = `${(-window.scrollY + 300) * .004}`
-// })
+window.addEventListener("scroll", () => {
+  document.querySelector(".banner-bg").style.backgroundSize = `${window.scrollY * 1.8 + 2000 }px`
+  document.querySelector(".banner-bg h1").style.opacity = `${(-window.scrollY + 300) * .004}`
+  document.querySelector(".banner-bg p").style.opacity = `${(-window.scrollY + 300) * .004}`
+  document.querySelector(".banner-bg i").style.opacity = `${(-window.scrollY + 300) * .004}`
+})
 
 // // bg stars
 
