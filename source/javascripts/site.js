504c8bd0bb9eadfe8e992648627db8b1932c1f45
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2024-07-27 14:17:42 +0900

    fixing effects

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -9,23 +9,23 @@ window.addEventListener("scroll", () => {
 
 // // bg stars
 
-function stars() {
-  let e = document.createElement("span");
-  e.setAttribute("class", "star");
-  document.body.appendChild(e);
-  e.style.left = Math.random() * + innerWidth + "px";
+// function stars() {
+//   let e = document.createElement("span");
+//   e.setAttribute("class", "star");
+//   document.body.appendChild(e);
+//   e.style.left = Math.random() * + innerWidth + "px";
 
-  let size = Math.random() * 12;
-  let duration = Math.random() * 3;
+//   let size = Math.random() * 12;
+//   let duration = Math.random() * 3;
 
-  e.style.fontSize = 12 + size + "px";
-  e.style.animationDuration = 2 + duration + "s";
+//   e.style.fontSize = 12 + size + "px";
+//   e.style.animationDuration = 2 + duration + "s";
 
-  setTimeout(() => {
-    document.body.removeChild(e);
-  }, 5000);
-}
+//   setTimeout(() => {
+//     document.body.removeChild(e);
+//   }, 5000);
+// }
 
-setInterval(() => {
- stars()
-}, 230);
+// setInterval(() => {
+//  stars()
+// }, 230);
